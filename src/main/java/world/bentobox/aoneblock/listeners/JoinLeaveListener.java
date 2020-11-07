5f7eb7e59cf35c90f1dbf4bfe994dfe883073433
tastybento <tastybento@wasteofplastic.com>
2020-11-07 12:28:29 -0800

    Add Join Leave Listener to save on player quit
    Other changes save every 5 blocks and when there is a new phase.
    https://github.com/BentoBoxWorld/AOneBlock/issues/129

---
+++ src/main/java/world/bentobox/aoneblock/listeners/JoinLeaveListener.java
@@ -0,0 +1,39 @@
+package world.bentobox.aoneblock.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import world.bentobox.aoneblock.AOneBlock;
+import world.bentobox.bentobox.database.objects.Island;
+import world.bentobox.bentobox.util.Util;
+
+public class JoinLeaveListener implements Listener {
+
+    AOneBlock addon;
+
+    /**
+     * @param addon
+     */
+    public JoinLeaveListener(AOneBlock addon) {
+        this.addon = addon;
+    }
+
+    /**
+     * Show particles when block is hit
+     * @param e - event
+     */
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onPlayerQuit(PlayerQuitEvent e) {
+        Island island = addon.getIslands().getIsland(addon.getOverWorld(), e.getPlayer().getUniqueId());
+        if (island != null) {
+            addon.getBlockListener().saveIsland(island).thenAccept(r -> {
+                if (!r) {
+                    addon.logError("Could not save AOneBlock island at " + Util.xyz(island.getCenter().toVector()) + " to database " + island.getUniqueId());
+                }
+            });
+        }
+    }
+
+}
