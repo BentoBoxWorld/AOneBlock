08535db2ff3f7312e6e4ee75d1769557091607e0
tastybento <tastybento@wasteofplastic.com>
2021-07-31 10:25:40 -0700

    Convert to record

--- src/main/java/world/bentobox/aoneblock/oneblocks/MobAspects.java
+++ src/main/java/world/bentobox/aoneblock/oneblocks/MobAspects.java
@@ -8,28 +8,4 @@ import org.bukkit.Sound;
  * @author tastybento
  *
  */
-public class MobAspects {
-
-    private final Sound sound;
-    private final Color color;
-    /**
-     * @param sound - sound to play
-     * @param color - color of particles to show
-     */
-    public MobAspects(Sound sound, Color color) {
-        this.sound = sound;
-        this.color = color;
-    }
-    /**
-     * @return the sound
-     */
-    public Sound getSound() {
-        return sound;
-    }
-    /**
-     * @return the color
-     */
-    public Color getColor() {
-        return color;
-    }
-}
+public record MobAspects(Sound sound, Color color) { }
