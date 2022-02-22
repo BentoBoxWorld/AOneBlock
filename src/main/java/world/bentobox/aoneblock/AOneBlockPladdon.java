04c71a05fd60c1da5f75f7b8097a26bb8dd53166
BONNe <bonne@bonne.id.lv>
2022-02-22 13:14:44 +0200

    Upgrade to BentoBox API 1.20.

--- src/main/java/world/bentobox/aoneblock/AOneBlockPladdon.java
+++ src/main/java/world/bentobox/aoneblock/AOneBlockPladdon.java
@@ -1,8 +1,16 @@
 package world.bentobox.aoneblock;
 
+import org.bukkit.plugin.java.annotation.dependency.Dependency;
+import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
+
 import world.bentobox.bentobox.api.addons.Addon;
 import world.bentobox.bentobox.api.addons.Pladdon;
 
+
+@Plugin(name="Pladdon", version="1.0")
+@ApiVersion(ApiVersion.Target.v1_17)
+@Dependency(value = "BentoBox")
 public class AOneBlockPladdon extends Pladdon {
 
     @Override
