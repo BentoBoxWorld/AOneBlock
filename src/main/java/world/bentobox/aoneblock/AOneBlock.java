BentoBoxWorld/AOneBlock

commit a50702961cf91cc02657575438fb08c0c8270a73

Add Hologram Support to AOneBlock Phases (#179)
* Add Holograms to AOneBlock Phases

* Add Holograms to AOneBlock Phases

* Fix Holograms Not Loading/Add Default Holograms & Fix Hologram Persistence

* Fix Merge of Develop, Github's Web Interface is Bad

* Convert to Addon#AllLoaded for Hologram Loading

src/main/java/world/bentobox/aoneblock/AOneBlock.java
@@ -3,10 +3,10 @@ package world.bentobox.aoneblock;
 import java.io.IOException;
 import java.util.Objects;
 
-import org.bukkit.World;
+import com.gmail.filoghost.holographicdisplays.api.Hologram;
+import com.gmail.filoghost.holographicdisplays.api.HologramsAPI;
+import org.bukkit.*;
 import org.bukkit.World.Environment;
-import org.bukkit.WorldCreator;
-import org.bukkit.WorldType;
 import org.bukkit.generator.ChunkGenerator;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
@@ -22,6 +22,7 @@ import world.bentobox.aoneblock.listeners.NoBlockHandler;
 import world.bentobox.aoneblock.oneblocks.OneBlocksManager;
 import world.bentobox.aoneblock.requests.IslandStatsHandler;
 import world.bentobox.aoneblock.requests.LocationStatsHandler;
+import world.bentobox.bentobox.BentoBox;
 import world.bentobox.bentobox.api.addons.GameModeAddon;
 import world.bentobox.bentobox.api.configuration.Config;
 import world.bentobox.bentobox.api.configuration.WorldSettings;
@@ -29,12 +30,14 @@ import world.bentobox.bentobox.database.objects.Island;
 
 /**
  * Main OneBlock class - provides an island minigame in the sky
+ *
  * @author tastybento
  */
 public class AOneBlock extends GameModeAddon {
 
     private static final String NETHER = "_nether";
     private static final String THE_END = "_the_end";
+    private static AOneBlock instance;
 
     // Settings
     private Settings settings;
@@ -43,9 +46,12 @@ public class AOneBlock extends GameModeAddon {
     private BlockListener blockListener;
     private OneBlocksManager oneBlockManager;
     private PlaceholdersManager phManager;
+    private Boolean useHolographicDisplays;
 
     @Override
     public void onLoad() {
+        // Set instance to this
+        instance = this;
         // Save the default config from config.yml
         saveDefaultConfig();
         // Load settings from config.yml. This will check if there are any issues with it too.
@@ -73,7 +79,7 @@ public class AOneBlock extends GameModeAddon {
     }
 
     @Override
-    public void onEnable(){
+    public void onEnable() {
         try {
             oneBlockManager = new OneBlocksManager(this);
             oneBlockManager.loadPhases();
@@ -92,24 +98,26 @@ public class AOneBlock extends GameModeAddon {
         registerListener(new JoinLeaveListener(this));
         // Register placeholders
         phManager = new PlaceholdersManager(this);
-        getPlugin().getPlaceholdersManager().registerPlaceholder(this,"visited_island_phase", phManager::getPhaseByLocation);
-        getPlugin().getPlaceholdersManager().registerPlaceholder(this,"visited_island_count", phManager::getCountByLocation);
-        getPlugin().getPlaceholdersManager().registerPlaceholder(this,"my_island_phase", phManager::getPhase);
-        getPlugin().getPlaceholdersManager().registerPlaceholder(this,"my_island_count", phManager::getCount);
-        getPlugin().getPlaceholdersManager().registerPlaceholder(this,"visited_island_next_phase", phManager::getNextPhaseByLocation);
-        getPlugin().getPlaceholdersManager().registerPlaceholder(this,"my_island_next_phase", phManager::getNextPhase);
-        getPlugin().getPlaceholdersManager().registerPlaceholder(this,"my_island_blocks_to_next_phase", phManager::getNextPhaseBlocks);
-        getPlugin().getPlaceholdersManager().registerPlaceholder(this,"visited_island_blocks_to_next_phase", phManager::getNextPhaseBlocksByLocation);
-        getPlugin().getPlaceholdersManager().registerPlaceholder(this,"my_island_percent_done", phManager::getPercentDone);
-        getPlugin().getPlaceholdersManager().registerPlaceholder(this,"visited_island_percent_done", phManager::getPercentDoneByLocation);
-        getPlugin().getPlaceholdersManager().registerPlaceholder(this,"my_island_done_scale", phManager::getDoneScale);
-        getPlugin().getPlaceholdersManager().registerPlaceholder(this,"visited_island_done_scale", phManager::getDoneScaleByLocation);
+        getPlugin().getPlaceholdersManager().registerPlaceholder(this, "visited_island_phase", phManager::getPhaseByLocation);
+        getPlugin().getPlaceholdersManager().registerPlaceholder(this, "visited_island_count", phManager::getCountByLocation);
+        getPlugin().getPlaceholdersManager().registerPlaceholder(this, "my_island_phase", phManager::getPhase);
+        getPlugin().getPlaceholdersManager().registerPlaceholder(this, "my_island_count", phManager::getCount);
+        getPlugin().getPlaceholdersManager().registerPlaceholder(this, "visited_island_next_phase", phManager::getNextPhaseByLocation);
+        getPlugin().getPlaceholdersManager().registerPlaceholder(this, "my_island_next_phase", phManager::getNextPhase);
+        getPlugin().getPlaceholdersManager().registerPlaceholder(this, "my_island_blocks_to_next_phase", phManager::getNextPhaseBlocks);
+        getPlugin().getPlaceholdersManager().registerPlaceholder(this, "visited_island_blocks_to_next_phase", phManager::getNextPhaseBlocksByLocation);
+        getPlugin().getPlaceholdersManager().registerPlaceholder(this, "my_island_percent_done", phManager::getPercentDone);
+        getPlugin().getPlaceholdersManager().registerPlaceholder(this, "visited_island_percent_done", phManager::getPercentDoneByLocation);
+        getPlugin().getPlaceholdersManager().registerPlaceholder(this, "my_island_done_scale", phManager::getDoneScale);
+        getPlugin().getPlaceholdersManager().registerPlaceholder(this, "visited_island_done_scale", phManager::getDoneScaleByLocation);
 
         // Register request handlers
         registerRequestHandler(new IslandStatsHandler(this));
         registerRequestHandler(new LocationStatsHandler(this));
-    }
 
+        // Decide if HolographicDisplays is Useable
+        useHolographicDisplays = Bukkit.getPluginManager().isPluginEnabled("HolographicDisplays");
+    }
 
     @Override
     public void onDisable() {
@@ -160,8 +168,9 @@ public class AOneBlock extends GameModeAddon {
 
     /**
      * Gets a world or generates a new world if it does not exist
-     * @param worldName2 - the overworld name
-     * @param env - the environment
+     *
+     * @param worldName2      - the overworld name
+     * @param env             - the environment
      * @param chunkGenerator2 - the chunk generator. If <tt>null</tt> then the generator will not be specified
      * @return world loaded or generated
      */
@@ -225,6 +234,20 @@ public class AOneBlock extends GameModeAddon {
     public void allLoaded() {
         // save settings. This will occur after all addons have loaded
         this.saveWorldSettings();
+
+        // Manage Old Holograms
+        if (useHolographicDisplays()) {
+            for (Island island : getIslands().getIslands()) {
+                OneBlockIslands oneBlockIsland = getOneBlocksIsland(island);
+                String hololine = oneBlockIsland.getHologram();
+                if (hololine != null) {
+                    final Hologram hologram = HologramsAPI.createHologram(BentoBox.getInstance(), island.getCenter().add(0.5, 2.6, 0.5));
+                    for (String line : hololine.split("\\n")) {
+                        hologram.appendTextLine(ChatColor.translateAlternateColorCodes('&', line));
+                    }
+                }
+            }
+        }
     }
 
     /**
@@ -249,11 +272,25 @@ public class AOneBlock extends GameModeAddon {
 
     /**
      * Get the placeholder manager
+     *
      * @return the phManager
      */
     public PlaceholdersManager getPlaceholdersManager() {
         return phManager;
     }
 
+    /**
+     * @return AOneBlock instance
+     */
+    public static AOneBlock getInstance() {
+        return instance;
+    }
+
+    /**
+     * @return whether to use Holographic Displays or Not
+     */
+    public boolean useHolographicDisplays() {
+        return useHolographicDisplays;
+    }
 
 }

src/main/java/world/bentobox/aoneblock/dataobjects/OneBlockIslands.java
@@ -28,6 +28,8 @@ public class OneBlockIslands implements DataObject {
     private long lifetime;
     @Expose
     private String phaseName = "";
+    @Expose
+    private String hologram = "";
 
     private List<OneBlockObject> queue = new ArrayList<>();
 
@@ -69,6 +71,20 @@ public class OneBlockIslands implements DataObject {
      */
     public void incrementBlockNumber() {
         this.blockNumber++;
+    }
+
+    /**
+     * @return the hologram Line
+     */
+    public String getHologram() {
+        return hologram;
+    }
+
+    /**
+     * @param hologramLine
+     */
+    public void setHologram(String hologramLine) {
+        this.hologram = hologramLine;
     }
 
     /* (non-Javadoc)

pom.xml
@@ -167,6 +167,13 @@
             <version>${spigot.version}</version>
             <scope>provided</scope>
         </dependency>
+        <!-- Holographic Displays API -->
+        <dependency>
+            <groupId>com.gmail.filoghost.holographicdisplays</groupId>
+            <artifactId>holographicdisplays-api</artifactId>
+            <version>2.4.0</version>
+            <scope>provided</scope>
+        </dependency>
         <!-- Mojang Auth-->
         <dependency>
             <groupId>com.mojang</groupId>

src/main/java/world/bentobox/aoneblock/oneblocks/OneBlockPhase.java
@@ -55,6 +55,7 @@ public class OneBlockPhase {
     private List<String> endCommands;
     private List<Requirement> requirements;
     private Map<Integer, OneBlockObject> fixedBlocks;
+    private Map<Integer, String> holograms;
 
     /**
      * Construct a phase that starts at blockNumber. Phase continues forever until
@@ -68,6 +69,7 @@ public class OneBlockPhase {
         endCommands = new ArrayList<>();
         requirements = new ArrayList<>();
         fixedBlocks = new HashMap<>();
+        holograms = new HashMap<>();
     }
 
     /**
@@ -84,6 +86,21 @@ public class OneBlockPhase {
      */
     public int getBlockNumberValue() {
         return Integer.parseInt(blockNumber);
+    }
+
+    /**
+     * @return the hologramLine
+     */
+    @Nullable
+    public String getHologramLine(Integer block) {
+        return holograms.getOrDefault(block, null);
+    }
+
+    /**
+     * @return the hologramLines
+     */
+    public Map<Integer, String> getHologramLines() {
+        return holograms;
     }
 
     /**
@@ -381,4 +398,10 @@ public class OneBlockPhase {
         this.fixedBlocks = fixedBlocks;
     }
 
+    /**
+     * @param hologramLines the hologramLines to set
+     */
+    public void setHologramLines(Map<Integer, String> hologramLines) {
+        this.holograms = hologramLines;
+    }
 }

src/main/java/world/bentobox/aoneblock/oneblocks/OneBlocksManager.java
@@ -39,6 +39,7 @@ public class OneBlocksManager {
     private static final String FIRST_BLOCK = "firstBlock";
     private static final String ICON = "icon";
     private static final String FIXED_BLOCKS = "fixedBlocks";
+    private static final String HOLOGRAMS = "holograms";
     private static final String CHESTS = "chests";
     private static final String RARITY = "rarity";
     private static final String CONTENTS = "contents";
@@ -190,6 +191,13 @@ public class OneBlocksManager {
             }
             addFixedBlocks(obPhase, phase.getConfigurationSection(FIXED_BLOCKS));
         }
+
+        if (phase.contains(HOLOGRAMS)) {
+            if (!obPhase.getHologramLines().isEmpty()) {
+                throw new IOException("Block " + blockNumber + ": Hologram Lines trying to be set to " + phase.getString(HOLOGRAMS) + " but already set to " + obPhase.getHologramLines() + " Duplicate phase file?");
+            }
+            addHologramLines(obPhase, phase.getConfigurationSection(HOLOGRAMS));
+        }
     }
 
     private void addFixedBlocks(OneBlockPhase obPhase, ConfigurationSection fb) {
@@ -219,6 +227,25 @@ public class OneBlocksManager {
         }
         // Store the remainder
         obPhase.setFixedBlocks(result);
+
+    }
+
+    private void addHologramLines(OneBlockPhase obPhase, ConfigurationSection fb) {
+        if (fb == null) return;
+        Map<Integer, String> result = new HashMap<>();
+        for (String key : fb.getKeys(false)) {
+            if (!NumberUtils.isNumber(key)) {
+                addon.logError("Fixed block key must be an integer. " + key);
+                continue;
+            }
+            int k = Integer.parseInt(key);
+            String line = fb.getString(key);
+            if (line != null) {
+                    result.put(k, line);
+            }
+        }
+        // Set Hologram Lines
+        obPhase.setHologramLines(result);
 
     }
 

src/main/resources/locales/en-US.yml
@@ -39,4 +39,5 @@ aoneblock:
     insufficient-permission: "&c You can proceed no futher until you obtain the [name] permission!"
   placeholders:
     infinite: Infinite
-    
+  island:
+    starting-hologram: "&aWelcome to AOneBlock\n&eBreak This Block to Begin"

src/main/resources/phases/0_plains.yml
@@ -15,6 +15,10 @@
     4: OAK_LOG
     5: OAK_LOG
     50: SPONGE
+  # Hologram Lines to Display
+  # The First (Before Phase 1) Hologram is Located in your Locale.
+  holograms:
+    0: "&aGood Luck!"
   biome: PLAINS
   # Commands
   # A list of commands can be run at the start and end of a phase. Commands are run as the Console

commit bebb0bf4f2b206db2e1f37928bc6ab3a91c7b88e

Fixes a bug where non-AOneBlock islands were stored
Commit a507029 introduced a bug where every island operated by the
server was loaded into the AOneBlock cache and a Hologram placed at the
center location of every island on the server even if the hologram was
blank. In writing the top ten addon for AOneBlock these erroneous
islands were discovered. This commit stops this from happening and
cleans up the database of all the unonwed and non-AOneBlock islands.

src/main/java/world/bentobox/aoneblock/AOneBlock.java
@@ -264,17 +264,19 @@ public class AOneBlock extends GameModeAddon {
 
         // Manage Old Holograms
         if (useHolographicDisplays()) {
-            for (Island island : getIslands().getIslands()) {
+            getIslands().getIslands().stream()
+            .filter(i -> this.inWorld(i.getWorld()))
+            .forEach(island -> {
                 OneBlockIslands oneBlockIsland = getOneBlocksIsland(island);
                 String hololine = oneBlockIsland.getHologram();
                 Location center = island.getCenter();
-                if (hololine != null) {
+                if (!hololine.isEmpty()) {
                     final Hologram hologram = HologramsAPI.createHologram(BentoBox.getInstance(), center.add(0.5, 2.6, 0.5));
                     for (String line : hololine.split("\\n")) {
                         hologram.appendTextLine(ChatColor.translateAlternateColorCodes('&', line));
                     }
                 }
-            }
+            });
         }
     }
 

src/main/java/world/bentobox/aoneblock/dataobjects/OneBlockIslands.java
@@ -93,6 +93,7 @@ public class OneBlockIslands implements DataObject {
     /**
      * @return the hologram Line
      */
+    @NonNull
     public String getHologram() {
         return hologram == null ? "" : hologram;
     }

src/main/java/world/bentobox/aoneblock/listeners/BlockListener.java
@@ -57,6 +57,7 @@ import world.bentobox.aoneblock.oneblocks.OneBlockPhase;
 import world.bentobox.aoneblock.oneblocks.OneBlocksManager;
 import world.bentobox.aoneblock.oneblocks.Requirement;
 import world.bentobox.bank.Bank;
+import world.bentobox.bentobox.api.events.BentoBoxReadyEvent;
 import world.bentobox.bentobox.api.events.island.IslandCreatedEvent;
 import world.bentobox.bentobox.api.events.island.IslandDeleteEvent;
 import world.bentobox.bentobox.api.events.island.IslandResettedEvent;
@@ -172,6 +173,23 @@ public class BlockListener implements Listener {
         handler = new Database<>(addon, OneBlockIslands.class);
         cache = new HashMap<>();
         oneBlocksManager = addon.getOneBlockManager();
+    }
+
+    /**
+     * This cleans up the cache files in the database and removed old junk.
+     * This is to address a bug introduced 2 years ago that caused non AOneBlock islands
+     * to be stored in the AOneBlock database. This should be able to be
+     * removed in the future.
+     * @deprecated will be removed in the future
+     * @param e event
+     */
+    @Deprecated(since = "1.12.3", forRemoval = true)
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    private void cleanCache(BentoBoxReadyEvent e) {
+        handler.loadObjects().forEach(i -> 
+            addon.getIslandsManager().getIslandById(i.getUniqueId())
+            .filter(is -> !addon.inWorld(is.getWorld()) || is.isUnowned())
+            .ifPresent(is -> handler.deleteID(is.getUniqueId())));
     }
 
     /**