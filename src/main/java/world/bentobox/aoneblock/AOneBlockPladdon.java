76c2bc869a457bb61601ac1e954120523369ee2b
tastybento <tastybento@wasteofplastic.com>
2023-03-25 09:45:16 -0700

    Switch to use annotations for plugin.yml

--- src/main/java/world/bentobox/aoneblock/AOneBlockPladdon.java
+++ src/main/java/world/bentobox/aoneblock/AOneBlockPladdon.java
@@ -7,8 +7,7 @@ import org.bukkit.plugin.java.annotation.plugin.Plugin;
 import world.bentobox.bentobox.api.addons.Addon;
 import world.bentobox.bentobox.api.addons.Pladdon;
 
-
-@Plugin(name="Pladdon", version="1.0")
+@Plugin(name="AOneBlock", version="1.0")
 @ApiVersion(ApiVersion.Target.v1_17)
 @Dependency(value = "BentoBox")
 public class AOneBlockPladdon extends Pladdon {
