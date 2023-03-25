2243385fb04d2f8a1761182dc134faa28bd0a626
tastybento <tastybento@wasteofplastic.com>
2023-03-25 09:53:15 -0700

    Remove dependency

--- src/main/java/world/bentobox/aoneblock/AOneBlockPladdon.java
+++ src/main/java/world/bentobox/aoneblock/AOneBlockPladdon.java
@@ -1,6 +1,5 @@
 package world.bentobox.aoneblock;
 
-import org.bukkit.plugin.java.annotation.dependency.Dependency;
 import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
 import org.bukkit.plugin.java.annotation.plugin.Plugin;
 
@@ -9,7 +8,6 @@ import world.bentobox.bentobox.api.addons.Pladdon;
 
 @Plugin(name="AOneBlock", version="1.0")
 @ApiVersion(ApiVersion.Target.v1_17)
-@Dependency(value = "BentoBox")
 public class AOneBlockPladdon extends Pladdon {
 
     @Override
